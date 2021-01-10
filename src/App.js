13c3d3e424fbf348d7a58c3a24f90f6eb761d2ef
brich33713 <brich33713@gmail.com>
2021-01-10 11:41:41 -0500

    adding files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DeckWrapper from './DeckWrapper.js';
+
+function App() {
+  return (
+    <div className="App">
+      <DeckWrapper />
+    </div>
+  );
+}
+
+export default App;
